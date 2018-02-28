b67ef912f94f718874c7cded11bb091b29276f5c
Cristian Anca <cristian.anca@gmail.com>
2018-02-28 17:17:44 +0200

    Fixed an import problem with newer versions of django

--- socialfeedsparser/models.py
+++ socialfeedsparser/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-from django.utils.importlib import import_module
+try:
+    from django.utils.importlib import import_module
+except ImportError:
+    from django.utils.module_loading import import_module
 from django.utils.translation import ugettext_lazy as _
 from django.utils.timezone import now
 from linkedin import linkedin
