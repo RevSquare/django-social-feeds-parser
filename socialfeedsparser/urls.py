5d257a9afcd2bc0d485fafba038ebc1b54d2fd6b
Cristian Anca <cristian.anca@gmail.com>
2018-03-09 12:30:34 +0200

    Removed patters from urls.py

--- socialfeedsparser/urls.py
+++ socialfeedsparser/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from .views import PostList, save_linkedin_token
 from .settings import PAGINATE_BY
 
-urlpatterns = patterns(
+urlpatterns = [
     '',
     url(r'^$', PostList.as_view(paginate_by=PAGINATE_BY), name="social_feeds_parser"),
     url(r'^linkedin-save-token/', save_linkedin_token, name='linkendin_save_token'),
-)
+]
